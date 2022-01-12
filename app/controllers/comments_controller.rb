f3289b70bb3ad66d241bac1e4cdd99b2096071f5
Cryp70-C0d3r <cryp70@tuta.io>
2022-01-11 22:07:12 -0600

    Working on viewing all users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,5 @@
 class CommentsController < ApplicationController
 
-  def create
-      @micropost = Micropost.find(params[:micropost])
-      @comment = @micropost.comments.create(params[:comment].permit(:username, :comment))
-      redirect_to current_user
-  end
 
-  def destroy
-      @micropost = Micropost.find(params[:micropost_id])
-      @comment = @micropost.comments.find(params[:id])
-      @comment.destroy
-      redirect_to current_user
-  end
-
-  def current_user?(user)
-    user == current_user
-  end
 
 end
